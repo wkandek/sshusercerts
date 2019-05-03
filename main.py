9e67c94ab1b43c482f41acb930b81e7ca77ad5f7
Wolfgang Kandek <wolfgang@kandek.com>
2019-05-03 04:35:22 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+from http.server import HTTPServer
+from server import Server
+
+HOST_NAME = 'caserver.kandek.com'
+PORT_NUMBER = 8080
+
+if __name__ == '__main__':
+	httpd = HTTPServer((HOST_NAME, PORT_NUMBER), Server)
+	print( time.asctime(), 'Server Up - %s:%s,' % (HOST_NAME, PORT_NUMBER))
+	try:
+		httpd.serve_forever()
+	except KeyboardInterrupt:
+		pass
+	httpd.server_close()
+	print( time.asctime(), 'Server Down - %s:%s,' % (HOST_NAME, PORT_NUMBER))
